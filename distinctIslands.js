ab625e014fdda2aaa57a22810cbf569222614eaf
Daniel Rowe <danielrowetech@gmail.com>
2020-04-30 10:53:30 -0700

    Register end of stack Update shape characters

--- distinctIslands.js
+++ distinctIslands.js
@@ -1,20 +1,26 @@
+// eslint-disable-next-line no-unused-vars
 const distinctIslands = (grid) => {
   const workingGrid = [...grid];
   const shapes = new Set();
   let currentShape;
 
   const flood = (rowIndex, colIndex, direction) => {
-    if (rowIndex < 0 || colIndex < 0) return;
-    if (rowIndex > grid.length - 1 || colIndex > grid[rowIndex].length - 1) return;
-    if (workingGrid[rowIndex][colIndex] === 0) return;
+    if (
+      (rowIndex < 0 || colIndex < 0) // Off the left side of map
+      || (rowIndex > grid.length - 1 || colIndex > grid[rowIndex].length - 1) // Off right side
+      || (workingGrid[rowIndex][colIndex] === 0) // Current tile is water
+    ) {
+      currentShape += '.'; // Register end of stack
+      return;
+    }
 
     workingGrid[rowIndex][colIndex] = 0;
     currentShape += direction;
 
-    flood(rowIndex, colIndex + 1, 'r');
-    flood(rowIndex + 1, colIndex, 'd');
-    flood(rowIndex, colIndex - 1, 'l');
-    flood(rowIndex - 1, colIndex, 'u');
+    flood(rowIndex, colIndex + 1, '>'); // Right
+    flood(rowIndex + 1, colIndex, 'v'); // Down
+    flood(rowIndex, colIndex - 1, '<'); // Left
+    flood(rowIndex - 1, colIndex, '^'); // Up
   };
 
   workingGrid.forEach((row, i) => {
