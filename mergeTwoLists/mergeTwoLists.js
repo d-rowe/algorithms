527774de4a01c540fd61e4323027af0ef4ee1051
Daniel Rowe <danielrowetech@gmail.com>
2020-05-05 20:31:26 -0700

    Clean and simplify

--- mergeTwoLists/mergeTwoLists.js
+++ mergeTwoLists/mergeTwoLists.js
@@ -1,41 +1,22 @@
 const mergeTwoLists = (l1, l2) => {
-  if (!l1 && !l2) return null;
-
+  const merged = { next: null };
+  let cn = merged;
   let n1 = l1;
   let n2 = l2;
-  const Node = (val = null, next = null) => ({ val, next });
-  const mergedList = Node();
-  let currN = mergedList;
 
-  const addToMerged = (n) => {
-    if (currN.val !== null) {
-      currN.next = Node(n.val);
-      currN = currN.next;
+  while (n1 && n2) {
+    if (n1.val > n2.val) {
+      cn.next = n2;
+      n2 = n2.next;
     } else {
-      currN.val = n.val;
+      cn.next = n1;
+      n1 = n1.next;
     }
 
-    if (n === n1) n1 = n1.next;
-    if (n === n2) n2 = n2.next;
-  };
-
-  while (n1 || n2) {
-    if (n1 && !n2) {
-      addToMerged(n1);
-      continue;
-    }
-
-    if (n2 && !n1) {
-      addToMerged(n2);
-      continue;
-    }
-
-    if (n1.val <= n2.val) {
-      addToMerged(n1);
-    } else {
-      addToMerged(n2);
-    }
+    cn = cn.next;
   }
 
-  return mergedList;
+  cn.next = n1 || n2;
+
+  return merged.next;
 };
